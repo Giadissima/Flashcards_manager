f18fed22a0bf81da4f74617e0043dfed2406df8c
Giadissima <giadamariastellabetti@gmail.com>
2025-09-21 12:09:02 +0200

    add again routes in navbar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { Routes } from '@angular/router';
 import { SetupTest } from './test/setup-test/setup-test';
 import { TestResult } from './test/test-result/test-result';
 import { TestRunner } from './test/test-runner/test-runner';
+import { ManageGroupsComponent } from './group/manage-groups.component';
+import { CreateGroupComponent } from './group/create-group.component';
+import { EditGroupComponent } from './group/edit-group.component';
+import { ManageSubjectsComponent } from './subject/manage-subjects.component';
+import { CreateSubjectComponent } from './subject/create-subject.component';
+import { EditSubjectComponent } from './subject/edit-subject.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,4 +20,10 @@ export const routes: Routes = [
   { path: 'setup-test', component: SetupTest },
   { path: 'test-runner', component: TestRunner },
   { path: 'test-result', component: TestResult },
+  { path: 'manage-groups', component: ManageGroupsComponent },
+  { path: 'create-group', component: CreateGroupComponent },
+  { path: 'edit-group/:id', component: EditGroupComponent },
+  { path: 'manage-subjects', component: ManageSubjectsComponent },
+  { path: 'create-subject', component: CreateSubjectComponent },
+  { path: 'edit-subject/:id', component: EditSubjectComponent },
 ];
