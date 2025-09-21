bbbe24bccae8537f2b80b79a3fcc0e96529d579a
Giadissima <giadamariastellabetti@gmail.com>
2025-09-21 19:48:37 +0200

    rename all functions: from "getAllFlashcards" to "getAll", "updateFlashcard" to  "update" and so on

--- client/src/app/flashcard/flashcard.service.ts
+++ client/src/app/flashcard/flashcard.service.ts
@@ -1,4 +1,4 @@
-import { PaginatedResponse, SimplePaginatedResponse } from '../models/http.dto';
+import { CardFilter, PaginatedResponse, SimplePaginatedResponse } from '../models/http.dto';
 
 import { Flashcard } from '../models/flashcard.dto';
 import { HttpClient } from '@angular/common/http';
@@ -16,30 +16,30 @@ private baseUrl = baseUrlAPI + 'flashcards/';
   constructor(private http: HttpClient, private restClient: RestClientService) {}
 
   // Legge tutte le flashcard
-  getAllFlashcards(filter: SimplePaginatedResponse): Promise<PaginatedResponse<Flashcard>> {
-    return this.restClient.get<PaginatedResponse<Flashcard>>(
-      this.baseUrl + 'all/',
-      filter
-    )
-  }
+  getAll(filter: CardFilter): Promise<PaginatedResponse<Flashcard>> {
+      return this.restClient.get<PaginatedResponse<Flashcard>>(
+        this.baseUrl + 'all/',
+        filter
+      );
+    }
 
   // Legge una singola flashcard
-  getFlashcardById(id: string): Promise<Flashcard> {
+  getById(id: string): Promise<Flashcard> {
     return this.restClient.get<Flashcard>(this.baseUrl + id);
   }
 
   // Crea una flashcard
-  createFlashcard(card: Flashcard): Promise<void> {
+  create(card: Flashcard): Promise<void> {
     return this.restClient.post(this.baseUrl, card);
   }
 
   // Modifica una flashcard
-  updateFlashcard(id: string, card: Flashcard): Promise<void> {
+  update(id: string, card: Flashcard): Promise<void> {
     return this.restClient.patch(this.baseUrl + id, card);
   }
 
   // Elimina una flashcard
-  deleteFlashcard(id: string): Promise<void>{
+  delete(id: string): Promise<void>{
     return this.restClient.delete<void>(this.baseUrl + id)
   }
 }
