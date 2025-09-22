fb14e1a64bb66c09fdda8964c41667eb4d20eb32
Giadissima <giadamariastellabetti@gmail.com>
2025-09-22 10:04:13 +0200

    add logic on create test

--- client/src/app/test/setup-test/setup-test.ts
+++ client/src/app/test/setup-test/setup-test.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Group } from '../../models/group.dto';
+import { GroupService } from '../../group/group.service';
+import { Router } from '@angular/router';
+import { Subject } from '../../models/subject.dto';
+import { SubjectService } from '../../subject/subject.service';
+
+export function atLeastOneValidator(controls: string[]): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const hasValue = controls.some(controlName => !!group.get(controlName)?.value);
+    return hasValue ? null : { atLeastOneRequired: true };
+  };
+}
 
 @Component({
   selector: 'app-setup-test',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './setup-test.html',
-  styleUrl: './setup-test.scss'
+  styleUrls: ['./setup-test.scss']
 })
-export class SetupTest {
+export class SetupTest implements OnInit {
+  testForm: FormGroup;
+  subjects: Subject[] = [];
+  groups: Group[] = [];
+  allGroups: Group[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private subjectService: SubjectService,
+    private groupService: GroupService,
+    private router: Router
+  ) {
+    this.testForm = this.fb.group({
+      subject_id: [null],
+      group_id: [null],
+      num: [10, [Validators.required, Validators.min(1)]]
+    }, { validators: atLeastOneValidator(['subject_id', 'group_id']) });
+  }
+
+  ngOnInit(): void {
+    this.subjectService.getAllSubjects({ limit: 50, skip: 0, sortDirection: 'asc', sortField: 'name' })
+      .then(response => this.subjects = response.data);
+
+    this.groupService.getAllGroups({ limit: 50, skip: 0, sortDirection: 'asc', sortField: 'name' })
+      .then(response => {
+        this.allGroups = response.data;
+        this.groups = response.data;
+      });
+
+    this.testForm.get('subject_id')?.valueChanges.subscribe(subjectId => {
+      if (subjectId) {
+        this.groups = this.allGroups.filter(g => (g.subject_id as Subject)?._id === subjectId);
+      } else {
+        this.groups = this.allGroups;
+      }
+      this.testForm.get('group_id')?.setValue(null);
+    });
+  }
+
+  startTest(): void {
+    if (this.testForm.valid) {
+      const { subject_id, group_id, num } = this.testForm.value;
+      const queryParams: any = { num };
+      if (subject_id) {
+        queryParams.subject_id = subject_id;
+      }
+      if (group_id) {
+        queryParams.group_id = group_id;
+      }
+      this.router.navigate(['/test-runner'], { queryParams });
+    }
+  }
 }
--- client/src/app/test/test-runner/test-runner.ts
+++ client/src/app/test/test-runner/test-runner.ts
@@ -1,11 +1,121 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { Flashcard } from '../../models/flashcard.dto';
+import { FlashcardService } from '../../flashcard/flashcard.service';
 
 @Component({
   selector: 'app-test-runner',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './test-runner.html',
-  styleUrl: './test-runner.scss'
+  styleUrls: ['./test-runner.scss']
 })
-export class TestRunner {
+export class TestRunner implements OnInit {
+  testForm: FormGroup;
+  flashcards: Flashcard[] = [];
+  currentCardIndex = 0;
+  showAnswerMap: Record<string, boolean> = {};
+  testFinished = false;
+  correctAnswers = 0;
+  incorrectAnswers = 0;
+  elapsedTime = '0';
+  private startTime = 0;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private flashcardService: FlashcardService,
+    private router: Router
+  ) {
+    this.testForm = this.fb.group({
+      answers: this.fb.array([])
+    });
+  }
+
+  ngOnInit(): void {
+    this.startTime = Date.now();
+    this.route.queryParams.subscribe(params => {
+      const groupId = params['group_id'];
+      const subjectId = params['subject_id'];
+      const num = params['num'];
+
+      if ((groupId || subjectId) && num) {
+        this.loadFlashcards({
+          group_id: groupId,
+          subject_id: subjectId,
+          limit: num
+        });
+      }
+    });
+  }
+
+  get answers(): FormArray {
+    return this.testForm.get('answers') as FormArray;
+  }
+
+  loadFlashcards(filters: { group_id?: string, subject_id?: string, limit: number }): void {
+    this.flashcardService.getAll({
+      group_id: filters.group_id,
+      subject_id: filters.subject_id,
+      limit: filters.limit,
+      random: true,
+      skip: 0,
+      sortDirection: 'asc',
+      sortField: '_id'
+    }).then(response => {
+      this.flashcards = response.data;
+      this.flashcards.forEach(() => {
+        this.answers.push(this.fb.group({ isCorrect: null }));
+      });
+    });
+  }
+
+  getCardColor(card: Flashcard): string {
+    if (card.group_id && typeof card.group_id !== 'string' && card.group_id.color) {
+      return card.group_id.color;
+    }
+    return 'blue';
+  }
+
+  seeAnswer(card: Flashcard): void {
+    if (!card._id) return;
+    this.showAnswerMap[card._id] = !this.showAnswerMap[card._id];
+  }
+
+  getCardButtonText(card: Flashcard): string {
+    if (!card._id) return '';
+    return this.showAnswerMap[card._id] ? 'Vedi domanda' : 'Vedi risposta';
+  }
+
+  nextCard(): void {
+    if (this.currentCardIndex < this.flashcards.length - 1) {
+      this.currentCardIndex++;
+    }
+  }
+
+  previousCard(): void {
+    if (this.currentCardIndex > 0) {
+      this.currentCardIndex--;
+    }
+  }
+
+  finishTest(): void {
+    const endTime = Date.now();
+    this.elapsedTime = ((endTime - this.startTime) / 1000 / 60).toFixed(2);
+    this.correctAnswers = this.answers.controls.filter(control => control.value.isCorrect === true).length;
+    this.incorrectAnswers = this.answers.controls.filter(control => control.value.isCorrect === false).length;
+    this.testFinished = true;
+
+    this.router.navigate(['/test-result'], {
+      state: {
+        correct: this.correctAnswers,
+        incorrect: this.incorrectAnswers,
+        elapsedTime: this.elapsedTime,
+        total: this.flashcards.length
+      }
+    });
+  }
 }
