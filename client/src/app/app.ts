c42539da116a384790065b438fcf7c32bec44ad1
Giadissima <giadamariastellabetti@gmail.com>
2025-09-23 14:57:38 +0200

    unused imports removed

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
-import { RouterLink, RouterOutlet } from '@angular/router';
-
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
