da2af029a0d8db52aae7c808adeed4caeadc0e37
Giadissima <giadamariastellabetti@gmail.com>
2026-03-14 11:19:22 +0100

    increased maximum response length

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,7 @@
 export const charMinLength = 2;
 export const titleMaxLength = 100;
 export const questionMaxLength = 700;
-export const answerMaxLength = 1000;
+export const answerMaxLength = 8000;
 export const idLength = 24;
 export const nameMaxLength = 30;
 
